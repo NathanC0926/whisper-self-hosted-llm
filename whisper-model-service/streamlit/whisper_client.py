9ad4e2f23a071d8900bbdc14e40d76956e07eaab
NathanC0926 <147186779+NathanC0926@users.noreply.github.com>
2024-04-24 17:00:59 -0400

    Update whisper_client.py

--- whisper-model-service/streamlit/whisper_client.py
+++ whisper-model-service/streamlit/whisper_client.py
@@ -1,49 +1,21 @@
 import streamlit as st
 import requests
 import os
-import logging
+import ffmpeg
 
-# Initialize logging
-logging.basicConfig(level=logging.ERROR, filename='app_error.log', filemode='w',
-                    format='%(name)s - %(levelname)s - %(message)s')
-
-# Set up the page
 st.set_page_config(page_title="Whisper Speech Recognition", page_icon=":studio_microphone:")
 st.title(":studio_microphone: Speech Recognition")
 st.markdown("Upload an audio file you wish to have translated")
-
-# Set up the endpoint
 endpoint = os.getenv("MODEL_ENDPOINT", default="http://0.0.0.0:8001/inference")
-
-# File uploader widget
 audio = st.file_uploader("", type=["wav"], accept_multiple_files=False)
-
-# Process the uploaded audio file
+# read audio file
 if audio:
     audio_bytes = audio.read()
     st.audio(audio_bytes, format='audio/wav', start_time=0)
     files = {'file': audio_bytes}
-
-    # Make the POST request within a try-except block
-    try:
-        response = requests.post(endpoint, files=files)
-        # Check if the response status code is 200 (OK)
-        if response.status_code == 200:
-            try:
-                response_json = response.json()
-                st.subheader("Translated Text")
-                st.text_area(label="Result", value=response_json['text'], height=300)
-            except ValueError as e:  # Includes JSONDecodeError
-                # Log the JSON decoding error
-                logging.error(f'JSON decoding failed: {e}')
-                st.error("Failed to decode the server response. The response was not valid JSON.")
-        else:
-            # Log the error
-            logging.error(f'Failed to get a valid response: Status Code {response.status_code}, Response: {response.text}')
-            st.error(f"Failed to get a valid response. The server returned status code {response.status_code}.")
-    except requests.exceptions.RequestException as e:
-        # Log the request exception
-        logging.error(f'Request exception: {e}')
-        st.error("Failed to send the request. Please check the server and the endpoint.")
+    response = requests.post(endpoint, files=files)
+    response_json = response.json()
+    st.subheader(f"Translated Text")
+    st.text_area(label="", value=response_json['text'], height=300)
 else:
     st.write("Input not provided")
